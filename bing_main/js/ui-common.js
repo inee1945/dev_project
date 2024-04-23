5e636cfb4711b9f3d706932c082224ff2c561aa9
first-repo <inee1945@naver.com>
2024-04-23 18:10:40 +0900

    academy 4-23

--- bing_main/js/ui-common.js
+++ bing_main/js/ui-common.js
@@ -1,3 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  //헤더호버
+  $(".header_sec02")
+    .on("mouseover", function () {
+      $(".header").addClass("on");
+    })
+    .on("mouseout", function () {
+      $(".header").removeClass("on");
+    });
 
-})
+  //언어토글
+  $(".lang").click(function () {
+    if ($(".lang>ul").css("display") === "block") {
+      $(".lang>ul").slideUp();
+    } else {
+      $(".lang>ul").slideDown();
+    }
+  });
+
+  //사이트맵토글
+
+  $(".hamberger").on("click", function () {
+    if ($(".header_sec02").css("display") === "block") {
+      $(".header_sec02").css("display", "none");
+      $(".hamberger_menu").addClass("on");
+    } else {
+      $(".header_sec02").css("display", "block");
+      $(".hamberger_menu").removeClass("on");
+    }
+  });
+});
