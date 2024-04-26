08a23a88d45f2cabe86e6499329c6371a035d9b8
first-repo <inee1945@naver.com>
2024-04-26 16:26:42 +0900

    academy 4-26

--- bing_main - 복사본/js/ui-common.js
+++ bing_main - 복사본/js/ui-common.js
@@ -1,60 +1,31 @@
 $(document).ready(function () {
   //헤더호버
-  $(".header_sec02")
+  $(".gnb>li")
     .on("mouseover", function () {
-      $(".header").addClass("on");
+      $("#header").addClass("on");
     })
     .on("mouseleave", function () {
-      $(".header").removeClass("on");
+      $("#header").removeClass("on");
     });
 
   //언어토글
   $(".lang").click(function () {
-    if ($(".lang>ul").css("display") === "block") {
-      $(".lang>ul").slideUp();
-    } else {
-      $(".lang>ul").slideDown();
-    }
+    $(this).toggleClass("on");
   });
 
   //사이트맵토글
 
   $(".hamberger").on("click", function () {
-    let headerH = $("#header").css("height");
-    if ($(".header_sec02").css("display") === "block") {
-      $(".hamberger").addClass("checked");
-      $(".header_sec02").css("display", "none");
-      $(".hamberger_menu").addClass("on");
-      $(".hm_close").css("display", "none");
-      $(".header").css("height", "100px");
+    $(this).toggleClass("checked");
+    if (!$(".hamberger").hasClass("checked")) {
+      $(".pc_ham").css("visibility", "hidden");
     } else {
-      $(".hamberger").removeClass("checked");
-      $(".header_sec02").css("display", "block");
-      $(".hamberger_menu").removeClass("on");
-      $(".header").css("height", "537px");
+      $(".pc_ham").css({
+        opacity: 1,
+        visibility: "visible",
+        transition: "all 0.5s",
+      });
     }
   });
   //메인 슬라이더
-  const mainSlider = new Swiper(".sec02_slider .swiper", {
-    // Optional parameters
-    loop: true,
-
-    slidesPerView: 7,
-    spaceBetween: 30,
-    // If we need pagination
-    pagination: {
-      // el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      //el: ".swiper-scrollbar",
-    },
-  });
 });
